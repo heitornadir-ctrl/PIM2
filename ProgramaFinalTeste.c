cd844331aa0fb15b12ea81795a7f84d375e9f0cb
Krigor-10 <Krigordesousa@gmail.com>
2025-11-25 20:50:47 -0300

    Delete ProgramaFinalTeste.c

--- ProgramaFinalTeste.c
+++
@@ -1,9 +0,0 @@
-#include "sistemaAcademico_unificado.h"
-
-int main()
-{
-    executarSistema();
-    return 0;
-}
-
-// // atualizado em 14/11 as 00:12 // implementar para professor selecionar arquivo pelo explorador
